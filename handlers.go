b63414f7f8f43894297c66f1c71b00037c0bbdcf
Alexis Bellet <info@alexis-bellet.com>
2022-04-06 14:47:58 -0400

    initial commit

---
+++ handlers.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"go-with-gin/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// albums slice to seed record album data.
+var albums = []models.Album{
+    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+    // albums slice to seed record album data.
+    c.IndentedJSON(http.StatusOK, albums)
+}
+
+func getAlbumByID(c *gin.Context) {
+    id := c.Param("id")
+    for _, a := range albums {
+        if a.ID == id {
+            c.IndentedJSON(http.StatusFound, a) 
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+func postAlbums(c *gin.Context) {
+    var newAlbum models.Album
+
+    if err := c.BindJSON(&newAlbum); err != nil {
+        return
+    }
+    
+    // Add the new album to the slice.
+    albums = append(albums, newAlbum)
+    c.IndentedJSON(http.StatusCreated, newAlbum)
+}
